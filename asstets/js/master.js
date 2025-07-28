804e3d56b64bca325b004fb78cc5a8830cb734e9
parsa <parsafarashah@gmail.com>
2025-07-28 16:37:33 +0330

    last commit

--- asstets/js/master.js
+++ asstets/js/master.js
@@ -1,13 +1,13 @@
 let menuOpen = document.getElementById("menu");
 let btnMenu = document.getElementById("btn");
 let closeBtn = document.getElementById("closeBtn");
-console.log(btnMenu);
+
+// open button
 
 btnMenu.addEventListener("click", () => {
   menuOpen.classList.add("open");
-  console.log("hello");
 });
+// close button
 closeBtn.addEventListener("click", () => {
   menuOpen.classList.remove("open");
-  console.log("hello");
 });
